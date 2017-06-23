1e8c74e4cbb21b568db64e35945c89e0737e9d76
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2017-06-23 13:35:30 +0200

    add possibility to generate hashes for inline style

--- middleware/csp.py
+++ middleware/csp.py
@@ -10,16 +10,20 @@ class CSPMiddleware:
         policy = {
             'default-src': ["'self'"],
             'img-src': ["'self'", "data:"],
-            'script-src': ["'self'"]
+            'script-src': ["'self'"],
+            'style-src': ["'self'"],
         }
         if hasattr(request, 'csp_nonces'):
             policy['script-src'] += ["'nonce-{}'".format(nonce) for nonce in request.csp_nonces]
+
+        if hasattr(request, 'csp_css_hashes'):
+            policy['style-src'] += ["'{}'".format(css_hash) for css_hash in request.csp_css_hashes]
 
         # Allow inline styles for Django's builtin 403/404/500 pages
         # (which are only shown in DEBUG mode anyway)
         if settings.DEBUG and response.status_code in (403, 404, 500):
             policy['script-src'].append("'unsafe-inline'")
-            policy['style-src'] = ["'self'", "'unsafe-inline'"]
+            policy['style-src'].append("'unsafe-inline'")
 
         if settings.CSP_REPORT_URL:
             policy['report-uri'] = [settings.CSP_REPORT_URL]
--- templatetags/csp.py
+++ templatetags/csp.py
@@ -1,3 +1,6 @@
+import hashlib
+from base64 import b64encode
+
 from django import template
 from django.utils.crypto import get_random_string
 
@@ -14,3 +17,16 @@ def csp_nonce(context=None):
             request.csp_nonces = []
         request.csp_nonces.append(nonce)
     return nonce
+
+
+@register.simple_tag(name='csp_css_hash', takes_context=True)
+def csp_scc_hash(context, *args):
+    css = ''.join(args).encode('utf-8')
+    css_hash = "sha256-{}".format(b64encode(hashlib.sha256(css).digest()).decode('utf-8'))
+    if hasattr(context, 'request'):
+        request = context['request']
+        if not hasattr(request, 'csp_css_hashes'):
+            request.csp_css_hashes = []
+        if css_hash not in request.csp_css_hashes:
+            request.csp_css_hashes.append(css_hash)
+    return css
