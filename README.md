b6662b84bfa94626b674f382841a7d8f930d1b0d
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2017-07-03 14:53:05 +0200

    mage csp_css_hash usable as block, as chrome does not support hashd inline style

--- README.md
+++ README.md
@@ -9,6 +9,34 @@ Requires Django >=1.10
 Add it to MIDDLEWARE not MIDDLEWARE_CLASSES
 TODO
 
+### CSS
+
+Example use of hashed inline style:
+
+    {% load csp %}
+    <style>{% scp_scc_hash %} /* have to be directly together, same as the closing tag, or else the hash is invalid */
+        td.style-class {
+            background-color: red;
+        }
+    {% end_scp_scc_hash %}</style>
+
+usage inside of style="..." attributes is not supported by chromium for now.
+
+### Javascript
+
+#### Nonces
+
+TODO: Change to hashes?
+
+TODO: Change Name
+
+Example:
+
+    {% load csp %}
+    <script nonce={% csp_nonce %}>
+        alert("bla")
+    </script>
+
 ## Config Values
 
 CSP_REPORT_URL = ""
--- templatetags/csp.py
+++ templatetags/csp.py
@@ -19,14 +19,26 @@ def csp_nonce(context=None):
     return nonce
 
 
-@register.simple_tag(name='csp_css_hash', takes_context=True)
-def csp_scc_hash(context, *args):
-    css = ''.join(args).encode('utf-8')
-    css_hash = "sha256-{}".format(b64encode(hashlib.sha256(css).digest()).decode('utf-8'))
-    if hasattr(context, 'request'):
-        request = context['request']
-        if not hasattr(request, 'csp_css_hashes'):
-            request.csp_css_hashes = []
-        if css_hash not in request.csp_css_hashes:
-            request.csp_css_hashes.append(css_hash)
-    return css
+def csp_css_hash(parser, token):
+    nodelist = parser.parse(('end_csp_css_hash',))
+    parser.delete_first_token()
+    return CspCssHash(nodelist)
+
+
+class CspCssHash(template.Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context):
+        css = self.nodelist.render(context)
+        css_hash = "sha256-{}".format(b64encode(hashlib.sha256(css.encode('utf-8')).digest()).decode('utf-8'))
+        if hasattr(context, 'request'):
+            request = context['request']
+            if not hasattr(request, 'csp_css_hashes'):
+                request.csp_css_hashes = []
+            if css_hash not in request.csp_css_hashes:
+                request.csp_css_hashes.append(css_hash)
+        return css
+
+
+register.tag('csp_css_hash', csp_css_hash)
