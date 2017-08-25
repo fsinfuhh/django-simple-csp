80a05fb0a5863812057a6811c373389f7b691d26
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2017-08-25 16:47:59 +0200

    initial version of setup, change format of readme to rst to be in line with pip packaging guidlines

--- README.rst
+++ README.rst
@@ -1,19 +1,23 @@
-# django-simple-csp
+django-simple-csp
+=================
 
 A simple Middleware for adding CSP headers and nonces in Django
 
-## Usage
+Usage
+=====
 
 Requires Django >=1.10
 
 Add it to MIDDLEWARE not MIDDLEWARE_CLASSES
-TODO
+TODO: discribe in more detail
 
-### CSS
 
-TODO: remove hash from the name of tag, include style tag
+CSS
+---
 
-Example use of hashed inline style:
+TODO: remove hash from the name of tag?
+
+Example use of hashed inline style::
 
     {% load csp %}
     {% csp_css_hash %}
@@ -24,20 +28,24 @@ Example use of hashed inline style:
 
 usage inside of style="..." attributes is not supported by chromium for now.
 
-### Javascript
+Javascript
+----------
 
-#### Nonces
+Nonces
+~~~~~~
 
 TODO: Change to hashes?
 
-Example:
+Example::
 
     {% load csp %}
     <script nonce={% csp_js_nonce %}>
         alert("bla")
     </script>
 
-## Config Values
+
+Config Values
+-------------
 
 CSP_REPORT_URL = ""
 The URl CSP errors should be reportet to, set to "" if not used, or do not define it.
---
+++ setup.py
@@ -0,0 +1,54 @@
+import sys
+import os
+import codecs
+from setuptools import setup, find_packages
+
+
+version = '0.1.dev1'
+
+
+def read(*parts):
+    filename = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding='utf-8') as fp:
+        return fp.read()
+
+
+install_requires = [
+    'Django>=1.11',
+]
+
+
+test_requires = [
+]
+
+
+setup(
+    name='django-simple-csp',
+    version=version,
+    description='Django Content Security Policy support.',
+    long_description=read('README.rst'),
+    author='Nils Rokita, Henning Pridöhl',
+    author_email='0rokita@informatik.uni-hamburg.de; pridoeh@informatik.uni-hamburg.de',
+    maintainer='Nils Rokita',
+    maintainer_email='0rokita@informatik.uni-hamburg.de',
+    url='https://github.com/fsinfuhh/django-simple-csp',
+    license='License :: OSI Approved :: MIT License',
+    packages=find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Framework :: Django',
+    ]
+)
